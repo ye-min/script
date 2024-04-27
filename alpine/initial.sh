9631632e4fc1ff2cdfa5e15e1def009cf0665721
ye-min <39592394+ye-min@users.noreply.github.com>
2024-04-27 10:49:14 +0800

    Create alpine initial.sh

---
+++ alpine/initial.sh
@@ -0,0 +1,14 @@
+sed -i '1s/^/#/' /etc/apk/repositories
+sed -i '2s/^/#/' /etc/apk/repositories
+sed -i '$a https://mirrors.tuna.tsinghua.edu.cn/alpine/v3.19/main' /etc/apk/repositories
+sed -i '$a https://mirrors.tuna.tsinghua.edu.cn/alpine/v3.19/community' /etc/apk/repositories
+
+apk update
+apk upgrade
+
+apk add --no-cache openssh
+ssh-keygen -A
+sed -i '$a PermitRootLogin yes' /etc/ssh/sshd_config
+sed -i '$a PasswordAuthentication yes' /etc/ssh/sshd_config
+
+passwd
